d35f624459565a52d001b72956a46f67b11b5e2e
Elaidzha1940 <eli080293@gmail.com>
2023-02-28 17:51:00 +0630

    App Lock

---
+++ OTP Code/OTP Code/OTP_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTP_CodeApp.swift
+//  OTP Code
+//
+//  Created by Elaidzha Shchukin on 28.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct OTP_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
